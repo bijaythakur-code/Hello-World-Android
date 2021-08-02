128fb39cb2fc4754d0f8a1ee07c1c7d56d750c0d
DESKTOP-1MGS9EP\Bijay <bijaythakur330@gmail.com>
2021-08-02 22:32:46 +0545

    initial commit

---
+++ app/src/main/java/biijay/com/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package biijay.com.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO: Create a login feature in the app
+        setContentView(R.layout.activity_main);
+    }
+}
